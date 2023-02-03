50dcef8f87ced13e90261dbaa0e4cc43cf46b7b1
Ehren Grenlund <egrenlund@clintonelectronics.com>
2023-02-03 10:20:14 -0600

    Added file upload endpoint as example

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,3 @@
 export * from "./User";
 export * from "./Test";
+export * from "./File";
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { userRouter, testRouter } from "API/Routes";
+import { userRouter, testRouter, fileRouter } from "API/Routes";
 import { API } from "API/Objects";
 
 const api = new API()
 	.registerDefaultPlugins()
-	.registerRoutes([ userRouter, testRouter ])
+	.registerRoutes([ userRouter, testRouter, fileRouter ])
 	.listen((error) => {
 		if (error) {
 			throw new Error(error);
